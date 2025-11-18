d5a92d204d98958cc05fc5eb61af3d482d59270b
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2025-11-18 12:03:22 -0600

    feat: rutas separadas en otro archivo

--- src/routes/routes.go
+++ src/routes/routes.go
@@ -1,1 +1,23 @@
 package routes
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+
+func SetupRouter() *gin.Engine{
+	r := gin.New()
+
+	r.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"SAS":"Down",
+	})
+	})
+	r.GET("/spotify/:contex", func(ctx *gin.Context) {
+		nombre := ctx.Param("context")
+		ctx.String(http.StatusOK, "Que es dios? %s", nombre)
+	})
+	return r
+}
