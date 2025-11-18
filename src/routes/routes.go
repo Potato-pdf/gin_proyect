e59e91cb5cf5dca582b13edd11e2fbabbb4eee2e
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2025-11-18 12:08:51 -0600

    grupos de rutas

--- src/routes/routes.go
+++ src/routes/routes.go
@@ -9,13 +9,13 @@ import (
 
 func SetupRouter() *gin.Engine{
 	r := gin.New()
-
-	r.GET("/", func(ctx *gin.Context) {
+	RoutesGroup:= r.Group("/prefijo")
+	RoutesGroup.GET("/", func(ctx *gin.Context) {
 		ctx.JSON(http.StatusOK, gin.H{
 			"SAS":"Down",
 	})
 	})
-	r.GET("/spotify/:contex", func(ctx *gin.Context) {
+	RoutesGroup.GET("/spotify/:contex", func(ctx *gin.Context) {
 		nombre := ctx.Param("context")
 		ctx.String(http.StatusOK, "Que es dios? %s", nombre)
 	})
