7c3455f31a11c8bee4095a8f75e974abdb55ce7d
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2025-11-20 13:28:40 -0600

    feat: ruta ajolote

--- src/routes/routes.go
+++ src/routes/routes.go
@@ -1,8 +1,6 @@
 package routes
 
 import (
-	"net/http"
-
 	"Gin/src/controller"
 
 	"github.com/gin-gonic/gin"
@@ -10,15 +8,8 @@ import (
 
 func SetupRouter(r *gin.Engine) {
 	RoutesGroup := r.Group("/prefijo")
-	RoutesGroup.GET("/", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{
-			"SAS": "Down",
-		})
-	})
-	RoutesGroup.GET("/spotify/:context", func(ctx *gin.Context) {
-		nombre := ctx.Param("context")
-		ctx.String(http.StatusOK, "Que es dios? %s", nombre)
-	})
-
+	RoutesGroup.GET("/", controller.GetPrefijos)
+	RoutesGroup.GET("/spotify/:context", controller.GetContext)
 	RoutesGroup.POST("/create", controller.CreatePrefijo)
+	RoutesGroup.POST("/ajolote", controller.CreateAjolote)
 }
