6e892bea2551a4f876687392dc6950c7051f077b
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2025-11-20 11:39:55 -0600

    feat: type routes

--- src/routes/routes.go
+++ src/routes/routes.go
@@ -3,15 +3,16 @@ package routes
 import (
 	"net/http"
 
+	"Gin/src/domain/types"
+
 	"github.com/gin-gonic/gin"
 )
 
-
 func SetupRouter(r *gin.Engine) {
-	RoutesGroup:= r.Group("/prefijo")
+	RoutesGroup := r.Group("/prefijo")
 	RoutesGroup.GET("/", func(ctx *gin.Context) {
 		ctx.JSON(http.StatusOK, gin.H{
-			"SAS":"Down",
+			"SAS": "Down",
 		})
 	})
 	RoutesGroup.GET("/spotify/:context", func(ctx *gin.Context) {
@@ -20,6 +21,14 @@ func SetupRouter(r *gin.Engine) {
 	})
 
 	RoutesGroup.POST("/create", func(ctx *gin.Context) {
-		var nuevoPrefijo  
+		var nuevoPrefijo types.Prefijo
+		if err := ctx.ShouldBindJSON(&nuevoPrefijo); err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": "Recibido",
+			"data":    nuevoPrefijo,
+		})
 	})
 }
