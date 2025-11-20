8927b8729f9faffd7f5119f685338a09aeb35368
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2025-11-20 13:28:29 -0600

    controller create axolot

--- src/controller/controller.go
+++ src/controller/controller.go
@@ -8,6 +8,7 @@ import (
 )
 
 var prefijos []types.Prefijo
+var ajolotes []types.Ajolotes
 
 func CreatePrefijo(c *gin.Context) {
 	var nuevoPrefijo types.Prefijo
@@ -17,4 +18,23 @@ func CreatePrefijo(c *gin.Context) {
 	}
 	prefijos = append(prefijos, nuevoPrefijo)
 	c.JSON(http.StatusCreated, nuevoPrefijo)
+}
+
+func GetPrefijos(c *gin.Context) {
+	c.JSON(http.StatusOK, prefijos)
+}
+
+func GetContext(c *gin.Context) {
+	context := c.Param("context")
+	c.String(http.StatusOK, "Que es dios? %s", context)
+}
+
+func CreateAjolote(c *gin.Context) {
+	var nuevoAjolote types.Ajolotes
+	if err := c.ShouldBindJSON(&nuevoAjolote); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	ajolotes = append(ajolotes, nuevoAjolote)
+	c.JSON(http.StatusCreated, ajolotes)
 }
