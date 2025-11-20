342b2f44dbb3705c9ac0d9a06adb98d05526c63f
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2025-11-20 13:06:13 -0600

    controller create prefijos

--- src/controller/controller.go
+++ src/controller/controller.go
@@ -1,7 +1,20 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"Gin/src/domain/types"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+var prefijos []types.Prefijo
 
 func CreatePrefijo(c *gin.Context) {
-
+	var nuevoPrefijo types.Prefijo
+	if err := c.ShouldBindJSON(&nuevoPrefijo); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	prefijos = append(prefijos, nuevoPrefijo)
+	c.JSON(http.StatusCreated, nuevoPrefijo)
 }
