0d2ff613f270edd9e8fa4d55eb1bd2124abba316
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2025-11-20 11:39:45 -0600

    feat : type prefijo

--- src/domain/types/type.structure.go
+++ src/domain/types/type.structure.go
@@ -1,6 +1,10 @@
 package types
 
 type Ajolotes struct {
-    Nombre string `json:"nombre"`
-    Color  string `json:"color"`
+	Nombre string `json:"nombre"`
+	Color  string `json:"color"`
+}
+
+type Prefijo struct {
+	Nombre string `json:"nombre"`
 }
