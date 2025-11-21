ab21c7d094e3144c7005bb9b82e7dea276377ebc
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2025-11-21 12:48:40 -0600

    task model

---
+++ src/domain/models/task/task.model.go
@@ -0,0 +1,11 @@
+package task
+
+import "gorm.io/gorm"
+
+type Task struct {
+	gorm.Model
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Completed   bool   `json:"completed"`
+	UserID      uint   `json:"user_id"`
+}
