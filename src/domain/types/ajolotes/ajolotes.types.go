9743138476eb1d28f364a941c573441a2f2c62a2
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2025-11-21 12:03:48 -0600

    ajolotes.types

---
+++ src/domain/types/ajolotes/ajolotes.types.go
@@ -0,0 +1,6 @@
+package types
+
+type Ajolotes struct {
+	Nombre string `json:"nombre"`
+	Color  string `json:"color"`
+}
--- src/domain/types/type.structure.go
+++ src/domain/types/type.structure.go
@@ -1,10 +1,5 @@
 package types
 
-type Ajolotes struct {
-	Nombre string `json:"nombre"`
-	Color  string `json:"color"`
-}
-
 type Prefijo struct {
 	Nombre string `json:"nombre"`
 }
