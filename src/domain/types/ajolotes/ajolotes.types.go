2651d480870865156a1606ff028179f51e62c39b
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2025-11-21 13:06:16 -0600

    fix: types

--- src/domain/types/ajolotes/ajolotes.types.go
+++ src/domain/types/ajolotes/ajolotes.types.go
@@ -1,6 +1,9 @@
 package types
 
+import types "Gin/src/domain/types/task"
+
 type Ajolotes struct {
-	Nombre string `json:"nombre"`
-	Color  string `json:"color"`
+	Nombre string       `json:"nombre"`
+	Color  string       `json:"color"`
+	Task   []types.Task `json:"task"`
 }
--- src/domain/types/users/user.types.go
+++ src/domain/types/users/user.types.go
@@ -1,11 +1,10 @@
 package types
 
-import "Gin/src/domain/models/task"
+import types "Gin/src/domain/types/task"
 
 type User struct {
-	ID       uint
 	Name     string
 	Email    string
 	Password string
-	Tasks    []task.Task
+	Tasks    []types.Task
 }
