4796e9ee679c13ccf0ce1a36ff546aa18af30842
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2025-11-21 12:48:53 -0600

    user types

--- src/domain/types/users/user.types.go
+++ src/domain/types/users/user.types.go
@@ -1,9 +1,11 @@
-package users
+package types
+
+import "Gin/src/domain/models/task"
 
 type User struct {
 	ID       uint
 	Name     string
 	Email    string
 	Password string
-	Tasks    []Task
+	Tasks    []task.Task
 }
