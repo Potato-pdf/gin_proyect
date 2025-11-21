a9895901d282df15f049ee64a9737cc271f89c27
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2025-11-21 11:42:07 -0600

    feat: database conection

--- main.go
+++ main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"Gin/db"
 	"Gin/src/routes"
+	"fmt"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-
+	DBConnection := db.DBConnection()
+	if DBConnection == nil {
+		panic("Failed to connect to database")
+	}
+	fmt.Println("Connected to database")
 	r := gin.New()
 	routes.SetupRouter(r)
 	r.Run(":3000")
